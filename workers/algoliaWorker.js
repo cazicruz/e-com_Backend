5a98e18ce3de3c5cd35963c25968501b4695e847
Onwuli <onwuliDavid8@gmail.com>
2025-09-26 21:51:25 +0100

    added algolia integration for product sync

--- workers/algoliaWorker.js
+++ workers/algoliaWorker.js
@@ -1,10 +1,15 @@
 // workers/algoliaWorker.ts
 import { Worker } from "bullmq";
-import { Redis } from "ioredis";
-const {connection} =require('../jobs/algoliaQueue')
+const IORedis = require("ioredis");
+// const {connection} =require('../jobs/algoliaQueue')
 import { addOrUpdateProduct, deleteProduct } from "../utils/algoliaSearch";
 
-const connection = new Redis(process.env.REDIS_URL!);
+const connection = new IORedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_POST,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+});
 
 const worker = new Worker(
   "algolia-sync",
