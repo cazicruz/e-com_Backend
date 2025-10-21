a29d15941223d3bfd794b5cd47ed55102803d763
Onwuli <onwuliDavid8@gmail.com>
2025-10-21 20:55:38 +0100

    pushing to prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,34 +1,35 @@
-import path from 'path';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+// ecosystem.config.js
 module.exports = {
   apps: [
     {
       name: 'api-server',
-      script: './app.js', // or app.js, index.js
+      script: './app.js',
       instances: 1,
       exec_mode: 'cluster',
       env: {
-        NODE_ENV: process.env.NODE_ENV || 'development',
-        PORT: process.env.PORT || 8000
+        NODE_ENV: 'production'
       },
       error_file: './logs/api-error.log',
       out_file: './logs/api-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss'
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: '1G'
     },
     {
       name: 'worker',
-      script: './worker/algoliaWorker.js',
+      script: './workers/algoliaWorker.js',
       instances: 1,
       exec_mode: 'fork',
       env: {
-        NODE_ENV: process.env.NODE_ENV || 'development'
+        NODE_ENV: 'production'
       },
       error_file: './logs/worker-error.log',
       out_file: './logs/worker-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss'
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: '512M'
     }
   ]
 };
