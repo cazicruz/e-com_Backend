e70dd50302f1eec19021d448fbf231282374324b
Onwuli <onwuliDavid8@gmail.com>
2025-10-24 09:40:02 +0100

    changed cart managaement to let unAuthenticated users have cart

--- models/Cart.js
+++ models/Cart.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const cartSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, 
     ref: 'User', 
-    // required: true 
   },
   items: [{
     productId: { type: mongoose.Schema.Types.ObjectId,
